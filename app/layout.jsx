c8c63c56ef805bbb1c95b9b9af879a31aba50d87
vann19 <ahmadfansya660@gmail.com>
2024-05-28 15:58:39 +0700

    first init project

---
+++ app/layout.jsx
@@ -0,0 +1,10 @@
+export default function Layout ({ children }) {
+    return (
+        <html lang="en">
+         <head>
+            <title>Belajar Next js di codepolitan</title>
+         </head>
+         <body>{children}</body>
+        </html>
+    )
+}
